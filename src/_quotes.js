cff46b3b84d57366dfa01f9931e6176304d3d6d2
datejer <ejer.priv@wp.pl>
2020-01-14 23:22:47 +0100

    Add some quotes

--- src/_quotes.js
+++ src/_quotes.js
@@ -1,1 +1,6 @@
-module.exports = [`hello`, `world`];
+module.exports = [
+	`Wszędzie w domu, ale dobrze najlepiej.`,
+	`Mam białego skina bo czarnego trzeba kupić.`,
+	`Wszystko ma swoje zady i walety.`,
+	`1C - jak cycki.`
+];
