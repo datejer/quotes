99fc303d4be6ed7be45811c6f1edfb97e89f81f7
datejer <ejer.priv@wp.pl>
2020-01-15 10:29:15 +0100

    Add quotes

--- src/_quotes.js
+++ src/_quotes.js
@@ -2,5 +2,7 @@ module.exports = [
 	`Wszędzie w domu, ale dobrze najlepiej.`,
 	`Mam białego skina bo czarnego trzeba kupić.`,
 	`Wszystko ma swoje zady i walety.`,
-	`1C - jak cycki.`
+	`1C - jak cycki.`,
+	`seks`,
+	`bajabongo wariaciku`
 ];
